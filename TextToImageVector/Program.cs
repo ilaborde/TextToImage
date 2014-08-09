60fb25e4eb1c5ed302a5deeb7b182144125afd77
ignacio laborde <laborde.ignacio@gmail.com>
2014-08-09 14:51:48 -0300

    added files

---
+++ TextToImageVector/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToImageVector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bitmapimage = new Bitmap(1000, 1000);
+            Graphics bitmapGraphics = Graphics.FromImage(bitmapimage);
+            bitmapGraphics.Clear(Color.CadetBlue);
+            bitmapGraphics.DrawString("ILaborde", new Font("Arial", 200,GraphicsUnit.Pixel), Brushes.Black, new RectangleF(10, 30,1000,1000));
+            bitmapimage.Save("Image.png", System.Drawing.Imaging.ImageFormat.Png);
+
+        }
+    }
+}
